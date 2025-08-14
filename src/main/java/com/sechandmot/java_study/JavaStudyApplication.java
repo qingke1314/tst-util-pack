6b67c4b045b3ce6f742fed3c70f1372a4de1a984
童圣滔 <tongshengtao13250@apexsoft.com.cn>
2025-08-14 22:39:21 +0800

    feat:get方法

--- src/main/java/com/sechandmot/java_study/JavaStudyApplication.java
+++ src/main/java/com/sechandmot/java_study/JavaStudyApplication.java
@@ -2,12 +2,21 @@ package com.sechandmot.java_study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class JavaStudyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaStudyApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(defaultValue = "World") String name, @RequestParam(defaultValue = "18") String age) {
+		return String.format("Hello %s, you are %s years old!", name, age);
+	}
 }
