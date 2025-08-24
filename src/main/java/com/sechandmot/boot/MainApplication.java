e00ba9c764a9915df56937354fd6ca1ccc9451be
童圣滔 <tongshengtao13250@apexsoft.com.cn>
2025-08-24 22:18:41 +0800

    feat:补充默认注解的注释

--- src/main/java/com/sechandmot/boot/MainApplication.java
+++ src/main/java/com/sechandmot/boot/MainApplication.java
@@ -10,8 +10,10 @@ import com.sechandmot.boot.config.MyConfig;
 /**
  * 主程序类
  * @SpringBootApplication 是一个组合注解，包含了 @Configuration、@EnableAutoConfiguration、@ComponentScan 三个注解
- * @Configuration 表示这是一个配置类，会被 Spring 扫描到
+ * @SpringBootConfiguration 表示这是一个配置类，会被 Spring 扫描到
  * @EnableAutoConfiguration 表示开启自动配置，会根据项目的依赖自动配置 Spring 容器
+ *    AutoConfigurationPackage 利用Register类给容器导入一系列组件 将main类所在包下的组件都导入进来
+ *    AutoConfigurationImportSelector 给容器导入127个自动配置组件(利用Conditional注解按需生效)
  * @ComponentScan 表示开启组件扫描，会扫描当前包及其子包下的所有组件
  */
 @SpringBootApplication
--- src/main/java/com/sechandmot/boot/controller/HelloController.java
+++ src/main/java/com/sechandmot/boot/controller/HelloController.java
@@ -2,11 +2,13 @@ package com.sechandmot.boot.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController // controller + responsebody
 public class HelloController {
   @RequestMapping("/hello")
-  public String hello() {
-    return "hello";
+  public String hello(@RequestParam(value = "name", defaultValue = "张三") String name ) {
+    // name 默认为张三
+    return "hello " + name;
   }
 }
